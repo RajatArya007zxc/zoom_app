52208f76aab78206a356621cc1a37c24dd5eda65
Rajat <rajatarya199zx@gmail.com>
2020-09-12 20:30:54 +0530

    create a room id

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express=require('express');
 const app =express();
 const server=require('http').Server(app);
+const {v4:uuidv4} =require('uuid');
 
 //for ejs
-
 app.set('view engine','ejs')
 
 //first url 
 app.get('/',(req,res)=>{
 
-  res.render('room');
+  res.redirect(`${uuidv4()}`)
 
+})
+
+
+app.get('/:room',(req,res)=>{
+
+    res.render('room',{roomId: req.params.room})
 })
 
 server.listen(3030);
