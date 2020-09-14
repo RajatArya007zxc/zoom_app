69f3dfb78fca9e6b2abac4c98e97bbfaa31ef816
Rajat <rajatarya199zx@gmail.com>
2020-09-15 03:21:23 +0530

    adding other user  with stream by peer and socket.io

--- public/script.js
+++ public/script.js
@@ -1,11 +1,17 @@
 
 
+const socket=io('/')
+
 const videoGrid=document.getElementById('video-grid')
-console.log(videoGrid);
+//console.log(videoGrid);
 const myVideo=document.createElement('video');
 myVideo.muted =true;
 
-
+const peer=new Peer(undefined,{
+    path:'/peerjs',
+    host:'/',
+    port:'3030'
+})
 
 
 
@@ -17,7 +23,48 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream=>{
     myVideoStream=stream;
     addVideoStream(myVideo,stream)
+
+peer.on('call',call=>{
+    call.answer(stream);
+    const video=document.createElement('video');
+call.on('stream',userVideoStream=>{
+    addVideoStream(video,userVideoStream)
 })
+})
+
+
+
+
+    socket.on('user-connected',(userId)=>{
+        connectNewUser(userId,stream)
+        })
+        
+})
+
+
+peer.on('open',id=>{
+    socket.emit('join-room',Room_id,id);
+})
+
+//socket.emit('join-room',Room_id,id);
+
+// socket.on('user-connected',(userId)=>{
+// connectNewUser(userId,stream)
+// })
+
+const connectNewUser=(userId,stream)=>{
+//console.log(userId) user ki id hai 
+const call=peer.call(userId,stream)
+const video=document.createElement('video')
+call.on('stream',userVideoStream=>{
+    addVideoStream(video,userVideoStream)
+})
+}
+
+
+////////////
+
+
 
 const addVideoStream=(video,stream)=>{
     video.srcObject=stream
--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express=require('express');
 const app =express();
 const server=require('http').Server(app);
 const {v4:uuidv4} =require('uuid');
+const io=require('socket.io')(server);
 
+
+const {ExpressPeerServer} =require('peer');
+const peerServer=ExpressPeerServer(server,{
+  debug:true
+})
+
+
+app.use('/peerjs',peerServer)
 app.use(express.static('public')) //this is used to public the script file
 
 
@@ -23,5 +32,17 @@ app.get('/:room',(req,res)=>{
 
     res.render('room',{roomId: req.params.room})
 })
+
+
+io.on('connection',socket=>{
+  socket.on('join-room',(room_Id,userId)=>{
+   socket.join(room_Id);
+   socket.to(room_Id).broadcast.emit('user-connected',userId);  //to inform other user to add that person on our stream
+  })
+})
+
+
+
+
 
 server.listen(3030);
